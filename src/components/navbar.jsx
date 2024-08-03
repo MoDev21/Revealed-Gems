ebe2cd6e273768b3742aa643dda4575729894198
Moïse Kaseka <e1444815@cmaisonneuve.qc.ca>
2024-08-03 00:37:47 -0400

    working on drop down functionnality

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,33 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import reactLogo from '../assets/react.svg'
 import './navbar.css';
 
-function DropdownMenu() {
-    return (
+
+
+
+
+function Navbar() {
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+
+    
+    const DropdownMenu = (
         <div className="dropdown_menu">
             <ul>
-                <li>web dev</li>
+                <li onClick={console.log('theme changed')}>web dev</li>
                 <li>mobile dev</li>
                 <li>design</li>
                 <li>more</li>
             </ul>
         </div>
     )
-}
 
-function Navbar() {
+    function DropdownMenu_drop(){
+        console.log('drop ' + isDropdownOpen);
+        setIsDropdownOpen(!isDropdownOpen);
+    }
+    
+
     return (
-        <nav className="navbar">
-            <img src={reactLogo} alt="logo" className="nav_logo" />
+        <>
+            <nav className="navbar">
+                <img src={reactLogo} alt="logo" className="nav_logo" />
 
-            <div className="button_row">
-                <input type="text" className="text" />
-                <button className='navbar_button'>Home</button>
-                <button className='navbar_button'>About</button>
-            </div>
-            <DropdownMenu />
-        </nav>
+                <div className="button_row">
+                    <input type="text" className="text" />
+                    
+                    <button className='navbar_button' onClick={() => DropdownMenu_drop()}>Home{isDropdownOpen ? DropdownMenu : ''}</button>
+                    
+                    <button className='navbar_button'>About</button>
+                </div>
+                
+            </nav>
+            
+        </>
+
     )
 }
 
