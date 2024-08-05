107090787e586fac699ba3245645b5e054696c19
Moïse Kaseka <e1444815@cmaisonneuve.qc.ca>
2024-08-04 20:01:32 -0400

    working on drop dowm functionnality v5

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -9,29 +9,39 @@ import './navbar.css';
 
 function Navbar() {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-    const dropDownMenuOptionsArray = ['web dev', 'mobile dev', 'design', 'more'];
+    const dropDownMenuCategoryArray = ['web dev', 'mobile dev', 'design', 'more'];
     const dropdownMenuSortOptionsArray = ['name ↥', 'name ↧', 'price ↥', 'price ↧', 'date ↥', 'date ↧'];
     const [buttonName, setButtonName] = useState('');
     // const dropdownMenu = isDropdownOpen? DropdownMenu : '';
+
+    const categoryMenuMapping = dropDownMenuCategoryArray.map((Option, index) => (
+        <li key={index} onClick={(e) => {
+            e.stopPropagation();
+            setIsDropdownOpen(!isDropdownOpen);
+            console.log('theme changed')
+        }}>{Option}</li>
+    ))
+
+    const sortMenuMapping = dropdownMenuSortOptionsArray.map((Option, index) => (
+        <li key={index} onClick={(e) => {
+            e.stopPropagation();
+            setIsDropdownOpen(!isDropdownOpen);
+        }}>{Option}</li>
+    ))
     
     const DropdownMenu = (
         <div className="dropdown_menu">
             <ul>
-                {dropDownMenuOptionsArray.map((Option, index) => (
-                    <li key={index} onClick={(e) => {
-                        e.stopPropagation();
-                        setIsDropdownOpen(!isDropdownOpen);
-                        console.log('theme changed')
-                    }}>{Option}</li>
-                ))}
+                {buttonName === 'category_dropdown' ? categoryMenuMapping : sortMenuMapping}
             </ul>
         </div>
     )
+    
 
     function DropdownMenu_drop(e){
         const buttonId = e.currentTarget.id;
-        console.log('buttonId: ' + buttonName);
         setButtonName(buttonId);
+        console.log('buttonId: ' + buttonId);
         console.log('drop ' + isDropdownOpen);
         setIsDropdownOpen(!isDropdownOpen);
     }
@@ -45,9 +55,9 @@ function Navbar() {
                 <div className="button_row">
                     <input type="text" className="text" />
                     
-                    <button className='navbar_button' id='category_dropdown' onClick={(e) => DropdownMenu_drop(e)}>Categories{isDropdownOpen ? DropdownMenu : ''}</button>
+                    <button className='navbar_button' id='category_dropdown' onClick={(e) => DropdownMenu_drop(e)}>Categories{isDropdownOpen && buttonName === 'category_dropdown' ? DropdownMenu : ''}</button>
                     
-                    <button className='navbar_button' id='sort_dropdown' onClick={(e) => DropdownMenu_drop(e)}>Sort</button>
+                    <button className='navbar_button' id='sort_dropdown' onClick={(e) => DropdownMenu_drop(e)}>Sort{isDropdownOpen && buttonName === 'sort_dropdown' ? DropdownMenu : ''}</button>
                 </div>
                 
             </nav>
