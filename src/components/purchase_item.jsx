e562448475346cc3e2a041dc96e7f2bf0aea4c5a
Moïse Kaseka <e1444815@cmaisonneuve.qc.ca>
2024-09-21 13:56:51 -0400

    modal box wip 2

--- src/components/purchase_item.jsx
+++ src/components/purchase_item.jsx
@@ -11,24 +11,33 @@ function Purchase_Item({ key, name, selectedName, setSelectedName, desc, price, 
         setIsItemToggled(!isItemToggled)
         setSelectedName(handledname)
         console.log(selectedName);
+        getMousePosition(event);
+
+        
     }
 
-    function getIndex(index) {
-        console.log(index);
-
+    const getMousePosition = (event) => {
+        // const { clientX, clientY } = event;
+        // console.log(clientX, clientY);
+        const x = event.clientX;
+        const y = event.clientY;
+        document.documentElement.style.setProperty('--mouse-x', `${x}px`);
+        document.documentElement.style.setProperty('--mouse-y', `${y}px`);
     }
-    
+
+
     return (
-        <div className={isItemToggled && selectedName === name ? 'purchase_item_modalbox' : 'purchase_item'} onClick={(event) => handleclickevent(event, name)}>
-            <img src={image} alt='gem_image' />
-            <div className="purchase_item__info_container">
-                <h2>{name}</h2>
-                <p>{desc}</p>
-                {/* <h3>{price}</h3> */}
-                <p>{categorie}</p>
-                {isItemToggled && selectedName === name ? <button>buy on Amazon</button> : ''}
+        <div className='purchase_item_container'>
+            <div className={isItemToggled && selectedName === name ? 'purchase_item_modalbox' : 'purchase_item'} onClick={(event) => handleclickevent(event, name)}>
+                <img src={image} alt='gem_image' />
+                <div className="purchase_item__info_container">
+                    <h2>{name}</h2>
+                    <p>{desc}</p>
+                    {/* <h3>{price}</h3> */}
+                    <p>{categorie}</p>
+                    {isItemToggled && selectedName === name ? <button>buy on Amazon</button> : ''}
+                </div>
             </div>
-
         </div>
     );
 }
